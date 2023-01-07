2867c3ba4476aef0fd3b6ce0ee70887200a1960e
shivoyraj <shivamrajtripathi@hotmail.com>
2023-01-08 01:15:32 +0530

    Meal Details functionality completed

--- script/meal.js
+++ script/meal.js
@@ -20,9 +20,30 @@ var loadData = async function () {
     let obj = await getDataByName(itemName)
     
     document.getElementById('mealHeading').innerHTML = obj.strMeal
+    document.getElementById('strMealThumb').src = obj.strMealThumb
     document.getElementById('idMeal').innerHTML = obj.idMeal
+    //document.getElementById(tagRow).innerHTML = ""
+    if(obj.strTags)
+        document.getElementById('strTags').innerHTML = obj.strTags
+    else{
+        document.getElementById('tagRow').remove()
+    }
+    
     document.getElementById('strCategory').innerHTML = obj.strCategory
     document.getElementById('strArea').innerHTML = obj.strArea
+    
+    document.getElementById('strYoutube').href = obj.strYoutube
+    document.getElementById('strYoutube').target = '_blank'
+
+    document.getElementById('strSource').href = obj.strSource
+    document.getElementById('strSource').target = '_blank'
+    
+    let ingredients = ''
+    for(let i=0; i<20 ; i++){
+        if(obj['strIngredient'+i])
+            ingredients = ingredients + i + ': ' + (obj['strIngredient'+i] +' '+ obj['strMeasure'+i]) + ', '
+    }
+    document.getElementById('ingredients').innerHTML = ingredients.slice(0,ingredients.length-2)
     document.getElementById('strInstructions').innerHTML = obj.strInstructions
 }
 
